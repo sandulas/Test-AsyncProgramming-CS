87c05e573127bbe993a0cb3025328de120f6d675
Adrian Sandu <adrian.sandu@pronetis.com>
2019-06-30 20:12:20 +0300

    added a CPU-bound method (calculate PI using Tayloe series)

--- Program.cs
+++ Program.cs
@@ -24,5 +24,23 @@ namespace AsynchronousProgrammingTest
 			await Task.Delay(5000);
 			return 1;
 		}
+
+		// compute PI using Taylor series: PI/4 = 1 - 1/3 + 1/5 - 1/7 + 1/9 - ... 
+		static void computePI()
+		{
+			double quarterPI = 1;
+			double taylorTerm;
+
+			for (int i = 0; i < 1E9; i++)
+			{
+				taylorTerm = (double)1 / (i * 2 + 3);
+
+				if (i % 2 == 0) taylorTerm = -taylorTerm;
+
+				quarterPI += taylorTerm;
+			}
+
+			Console.WriteLine($"Computed PI: { quarterPI * 4 }; Math.PI: { Math.PI }");
+		}
 	}
 }
